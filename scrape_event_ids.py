9ac1027806a06d5c8b761fed9f79518c34729e74
leo-pfeiffer <leopold.pfeiffer@icloud.com>
2025-06-23 12:31:15 -0400

    Fix script further

--- scrape_event_ids.py
+++ scrape_event_ids.py
@@ -10,6 +10,10 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 import time
 
+IGNORE = {
+    "2024 Doha All Women's Race"  # not available
+}
+
 
 def get_driver():
     chromedriver_path = '/Users/leopoldpfeiffer/WebDriver/chromedriver'  # change path as needed
@@ -18,12 +22,11 @@ def get_driver():
 
     options = Options()
     options.add_argument('--disable-notifications')
-    # options.add_argument("--headless=new")
 
-    driver = webdriver.Chrome(service=service, options=options)
-    driver.implicitly_wait(20)
-    driver.set_script_timeout(20)
-    return driver
+    _driver = webdriver.Chrome(service=service, options=options)
+    _driver.implicitly_wait(20)
+    _driver.set_script_timeout(20)
+    return _driver
 
 
 def scrape_ids(driver: WebDriver, event_ids: list[tuple[str, str]]):
@@ -37,8 +40,12 @@ def scrape_ids(driver: WebDriver, event_ids: list[tuple[str, str]]):
     all_event_options = Select(events).options
     event_option_values = [opt.get_attribute("value") for opt in all_event_options]
 
-    for event_name in event_option_values[21:]:
-        driver.find_element(By.CSS_SELECTOR, f"option[value^='{event_name}']").click()
+    for event_name in event_option_values:
+
+        if event_name in IGNORE:
+            continue
+
+        driver.find_element(By.CSS_SELECTOR, f"option[value^=\"{event_name}\"]").click()
 
         time.sleep(1)
 
@@ -52,12 +59,9 @@ def scrape_ids(driver: WebDriver, event_ids: list[tuple[str, str]]):
 
         time.sleep(1)
 
-        try:
-            event_id = driver.current_url.split("?event=")[1].split("&")[0].split("_")[1]
-            event_ids.append((event_name, event_id))
-            print(f"{event_id} – {event_name}")
-        except Exception:
-            logging.error(f"Could not get event ID for {event_name}")
+        event_id = driver.current_url.split("?event=")[1].split("&")[0].split("_")[1]
+        event_ids.append((event_name, event_id))
+        print(f"{event_id} – {event_name}")
 
         driver.back()
         driver.back()
@@ -76,6 +80,6 @@ if __name__ == '__main__':
         scrape_ids(driver, event_ids)
     except Exception as e:
         logging.error(e)
-    print(event_ids)
+        logging.error(e.__traceback__)
     write_csv(event_ids)
     driver.quit()
